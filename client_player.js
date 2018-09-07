20fc08a6c8af51c0d9f243b791136db448472274
Daniel Margulis <damargulis1@gmail.com>
2018-09-07 17:16:05 -0400

    .

--- client_player.js
+++ client_player.js
@@ -200,15 +200,15 @@ Player.prototype.onContentResumeRequested_ = function() {
   console.log(this.request_);
   this.broadcast_('onContentResumeRequested');
 
-  this.playerManager_.load(this.request_)
-  this.seek_(this.currentContentTime_);
-  //console.log('starting sleep 1');
-  //sleep(10000).then(() => {
-  //  console.log('ending sleep 1');
-  //  this.playerManager_.load(this.request_).then(() => {
-  //    this.seek_(this.currentContentTime_);
-  //  });
-  //});
+  //this.playerManager_.load(this.request_)
+  //this.seek_(this.currentContentTime_);
+  console.log('starting sleep 1');
+  sleep(10000).then(() => {
+    console.log('ending sleep 1');
+    this.playerManager_.load(this.request_).then(() => {
+      this.seek_(this.currentContentTime_);
+    });
+  });
 };
 
 /**
