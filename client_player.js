f849a2bcde229134a51a385ebbd2e52ba19dcdf7
Daniel Margulis <damargulis1@gmail.com>
2018-09-11 15:29:05 -0400

    .

--- client_player.js
+++ client_player.js
@@ -219,7 +219,7 @@ Player.prototype.onContentResumeRequested_ = function() {
   //});
 
   console.log('starting sleep 1');
-  sleep(10000).then(() => {
+  sleep(1000).then(() => {
     console.log('ending sleep 1');
     this.playerManager_.load(this.request_).then(() => {
       this.seek_(this.currentContentTime_);
